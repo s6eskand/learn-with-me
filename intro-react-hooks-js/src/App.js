a6d0ea5d001d74920ceb9ae7e8e591901882a142
Sam <sam.eskandar00@hotmail.com>
2020-06-19 23:23:35 -0400

    remove unnecessary imports

--- intro-react-hooks-js/src/App.js
+++ intro-react-hooks-js/src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // base styles
+import 'semantic-ui-css/semantic.min.css' // default Semantic UI theme
+import ListMovies from "./components/ListMovies"; // ListMovies Component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ListMovies title="test" />
     </div>
   );
 }
