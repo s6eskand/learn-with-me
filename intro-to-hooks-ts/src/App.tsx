83bf1569030e533201b6bc85da6e606731d5a83d
Sam <sam.eskandar00@hotmail.com>
2020-08-09 22:17:37 -0400

    update base

--- intro-to-hooks-ts/src/App.tsx
+++ intro-to-hooks-ts/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+
+import ListMovies from "./components/ListMovies";
 
 function App() {
   return (
     <div>
+      <ListMovies/>
     </div>
   );
 }
